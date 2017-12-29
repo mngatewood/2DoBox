5dfe76fabc60ffc406982ec1eec9890bc3c3f5c7
Charles Yach <CharlesY712@gmail.com>
2017-12-28 21:31:54 -0700

    Adding show completed tasks functionality

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ Card.prototype.createCard = function () {
     </form>
     <hr>
     </article>`);
-}
+};
 
 var qualityArray = ['none', 'low', 'normal', 'high', 'critical'];
 
@@ -26,9 +26,9 @@ function Card (title, body, uniqueId, quality, complete, textDeco) {
  this.uniqueId = uniqueId || $.now();
  this.body = body;
  this.quality = quality || 0;
- this.complete = complete;
+ this.complete = complete || ' ';
  this.textDeco = textDeco;
-}
+};
 
 function resetInputField () {
   $('#title-input').val('');
@@ -36,48 +36,47 @@ function resetInputField () {
   $('#title-input').focus();
   disableSaveButton();
   disableSeeMoreButton();
-}
+};
 
 window.onload = function() {
   persistIdea();
   disableSaveButton();
   disableSeeMoreButton();
-}
+};
 
 function persistIdea() {
   for(i = 0; i < localStorage.length; i++) {
     var getObject = localStorage.getItem(localStorage.key(i));
     var obj = JSON.parse(getObject);
     var persistCard = new Card(obj.title, obj.body, obj.uniqueId, obj.quality, obj.complete, obj.textDeco);
-    console.log(persistCard.complete)
-    if (persistCard.complete == 'checked') {    
-    } else {
+    if (persistCard.complete == ' ') {    
     persistCard.createCard();
-    }
-  }
-}
+    };
+  };
+};
 
-$('#search-input').on('keyup', function() {
-  var searchRequest = $('#search-input').val();
-  $('.task-element').each(function(){
-    var searchResult = $(this).text().indexOf(searchRequest);
-    this.style.display = searchResult > -1 ? "" : "none";
-  })
-})
-
-
+function persistCompletedIdea() {
+  for(i = 0; i < localStorage.length; i++) {
+    var getObject = localStorage.getItem(localStorage.key(i));
+    var obj = JSON.parse(getObject);
+    var persistCard = new Card(obj.title, obj.body, obj.uniqueId, obj.quality, obj.complete, obj.textDeco);
+    if (persistCard.complete == 'checked') {    
+    persistCard.createCard();
+    };
+  };
+};
 
 function stringToStorage(object) {
   var stringifyObject = JSON.stringify(object);
   localStorage.setItem(object.uniqueId, stringifyObject);
-}
+};
 
 function parseFromStorage(object) {
   var key = $(object.target).closest('article').attr('id');
   var retrievedIdea = localStorage.getItem(key);
   var parsedIdea = JSON.parse(retrievedIdea);
   return parsedIdea;
-}
+};
 
 
 
@@ -95,8 +94,8 @@ function upVoteStorage (event, object) {
   } else if ($quality.text() === 'high') {
     object['quality'] = 4;
     stringToStorage(object);
-  }
-}
+  };
+};
 
 function upVotePage (event, object) {
   var $quality = $(event.target).siblings('.importance-value');
@@ -112,8 +111,8 @@ function upVotePage (event, object) {
   } else if ($quality.text() === 'high') {
     $quality.text(qualityArray[4]);
     stringToStorage(object);
-  }
-}
+  };
+};
 
 function downVoteStorage (event, object) {
   var $quality = $(event.target).siblings('.importance-value');
@@ -129,8 +128,8 @@ function downVoteStorage (event, object) {
   } else if ($quality.text() === 'low') {
     object['quality'] = 0;
     stringToStorage(object);
-  }
-}
+  };
+};
 
 function downVotePage (event, object) {
   var $quality = $(event.target).siblings('.importance-value');
@@ -150,20 +149,20 @@ function disableSaveButton() {
     $('#save-button').prop('disabled', true);
   } else {
     $('#save-button').prop('disabled', false);
-  }
-}
+  };
+};
 
 function disableSeeMoreButton() {
   if (localStorage.length > 10) {
     $('#show-more-button').prop('disabled', false);
   } else {
     $('#show-more-button').prop('disabled', true);
-  }
-}
+  };
+};
 
 function toggleFilterButton() {
     $(event.target).toggleClass('inactive');
-}
+};
 
 $('#save-button').on('click', function(event){
   if ($('#title-input').val() == "" || $('#task-input').val() == ""){
@@ -175,15 +174,15 @@ $('#save-button').on('click', function(event){
     stringToStorage(newCard);
     disableSaveButton();
     resetInputField();
-  }
-})
+  };
+});
 
 $('#task-container').on('click', function(event) {
   if (event.target.className === 'delete-button') {
     var ideaId = event.target.closest('.task-element').id;
     $(`#${ideaId}`).remove();
     localStorage.removeItem(ideaId);
-  }
+  };
   disableSeeMoreButton();
 
 });
@@ -227,20 +226,20 @@ $('#search-input').on('keyup', function() {
   $('.task-element').each(function(){
     var searchResult = $(this).text().indexOf(searchRequest);
     this.style.display = searchResult > -1 ? "" : "none";
-  })
-})
+  });
+});
 
 $('#title-input').on('keyup', function () {
   disableSaveButton();
-})
+});
 
 $('#task-input').on('keyup', function () {
   disableSaveButton();
-})
+});
 
 $('.importance-filter-button').on('click', function(event) {
   toggleFilterButton();
-})
+});
 
 $('#task-container').on('click', '.task-complete', function(event) {
   var parsedIdea = parseFromStorage(event);
@@ -254,5 +253,13 @@ $('#task-container').on('click', '.task-complete', function(event) {
     parsedIdea['textDeco'] = 'none';
     $(this).parent('form').siblings('h2, p').css('text-decoration', 'none');
     stringToStorage(parsedIdea);
-  }
-})
+  };
+});
+
+$('#show-completed-tasks').on('click', function(){
+  if (this.checked) {
+    persistCompletedIdea();
+  } else {
+    location.reload();
+  };
+});
